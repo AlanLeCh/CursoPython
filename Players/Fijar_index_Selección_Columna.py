2fd693ec107f468a2f66dc7ef2f9a20ccbce4947
Alan Perales <alan.perales@imedmex.com.mx>
2025-05-06 13:44:50 -0600

    Se agregan dos archivos nuevos al curso de Python

--- Players/Fijar_index_Selección_Columna.py
+++ Players/Fijar_index_Selección_Columna.py
@@ -15,3 +15,75 @@ print(Player)
 
 #Seleccionar una columnas
 Player[['player_url', 'age', 'dob', 'height_cm', 'weight_kg', 'nationality', 'club']]
+
+
+                                                            #1 Seleccionar con un valor LOC[NOMBRE_FILA, NOMBRE_COLUMNA]
+#Obtener toda la data sobre L.Messi.
+
+Player.loc['L. Messi']
+
+#Obtener el tamaño de L.Messi.
+
+Player.loc['L. Messi', 'height_cm']
+
+#Obtener el peso de Cristiano Ronaldo.
+
+Player.loc['Cristiano Ronaldo', 'weight_kg']
+
+#Obtener todas las filas dentro de columna 'height_com'
+
+Player.loc[:, 'height_cm']
+
+#Obtener todas las columnas que correspondan al index 'L.Messi'
+
+Player.loc['L. Messi', :]
+
+
+                                                            # 2. Seleccionar con una lista de valores 
+
+#Obtener data sobre L. Messi y Cristiano Ronaldo.
+
+Player.loc[['L. Messi','Cristiano Ronaldo']]
+
+#Obtener el tamaño de L. Messi y Cristiano Ronaldo.
+
+Player.loc[['L. Messi', 'Cristiano Ronaldo'], 'height_cm']
+
+#Obtener el tamaño y peso de L. Messi
+
+Player.loc['L. Messi', ['height_cm', 'weight_kg']]
+
+#Obtener el tamaño y peso de L. Messi y Cristiano Ronaldo.
+
+Player.loc[['L. Messi', 'Cristiano Ronaldo'], ['height_cm', 'weight_kg']]
+
+
+                                                        # 3. Seleccionar un rango de data coon un slice (rebanada)
+                                            #START:STOP:STEP (NOTA: Contrariamente a las rebanadas tipicas en Python, "start" y "stop" en este caso son incluidos)
+
+#Hacer slice a nombre de columnas
+jugadores = ['L. Messi', 'Cristiano Ronaldo']
+Player.loc[jugadores, 'age':'club']
+
+#Hacer slice a los nombres de los index
+columnas = ['age', 'dob', 'height_cm','weight_kg']
+
+#Obtener los nombres de jugadores top1 y top10
+
+Player.index[:10]
+
+Player.loc['L. Messi':'M. Salah',columnas]
+
+                                                        # 4 Seleccionar con Condiciones
+#Una condición: seleccionar jugadores con tamaño arriba de 180cm
+columna = ['age', 'dob', 'height_cm','weight_kg']
+
+Player.loc[Player['height_cm'] >180, columna]
+
+
+#Multiples condicones: Seleccionar jugadores con tamaño arriba de 180cm y que sean de Argentina
+
+Player['height_cm'] >180
+Player['nationality'] == 'Argentina'
+
+Player.loc[(Player['height_cm'] > 180) & (Player['nationality'] == 'Argentina'), :]
