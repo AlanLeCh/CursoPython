0ef00542cea801d1ca1f35dbdee20dfd0aa8c94b
Alan Perales <alan.perales@imedmex.com.mx>
2025-12-09 16:59:10 -0600

    Se realizo las graficas de las Visualizaciones Interactivas.

--- Pivote/Visualización_Interactivas_Con_Panda.py
+++ Pivote/Visualización_Interactivas_Con_Panda.py
@@ -1,50 +1,73 @@
-#Importando librerias pandas 
+
+
+#Importando librerias
 import pandas as pd
-
-#importando libreria plotly
 import plotly.express as px
-import plotly.graph_objects as go
+#Configurar pandas para usar plotly como backend de gráficos
+pd.options.plotting.backend = "plotly"
 
-#importando libreria cufflinks
-import cufflinks as cf
-from  IPython.display import display,HTML
-cf.set_config_file (sharing='public',theme='white',offline=True)
-
-#Mandaos llamar al archivo csv
+#leerr archivo csv
 df_population = pd.read_csv("C:/Users/alanp/Desktop/CursoPython/Pivote/Proyecto/population_total.csv")
-#mostrar el archivo
+
+#mostrar resultados
 print(df_population)
 
-#Limpiando datos nulos
-df_population = df_population.dropna()
+#Eliminar valores NULL
+df_population = pd.read_csv("C:/Users/alanp/Desktop/CursoPython/Pivote/Proyecto/population_total.csv")
+df_population.dropna()
+df_population = df_population.pivot_table(index='year', columns='country', values='population')
 
-#colocar los años en el indice y los paises en las columnas
-df_population = df_population.pivot(index='year', columns='country' ,values='population')
-
-#Mostrar resultado
+#mostrar resultados
 print(df_population)
 
-#Mostando los paises seleccionados
-df_population = df_population [['United States', 'India', 'China', 'Indonesia', 'Brazil']]
-
-#mostrar resultado
-print (df_population)
-
-##########################################################################################################
-                                                 #GRAFICO DE LINEPLOT
-##########################################################################################################
-
-fig_line = px.line(df_population.iplot(kind='line',xtile='Year',ytitle='Population',title= 'Year vs Population'))
-
-#Mostrar el resultado
-fig_line.show()
+#Seleccionar paises
+df_population = df_population[['United States', 'India', 'China', 'Indonesia', 'Afghanistan']]   
+#Mostrar resultados de los paises seleccionados
+print(df_population)
 
 ##########################################################################################################
-                                                    #GRAFICO DE BARPLOT
+                                               #Grafico de Líneas (lineplot)
 ##########################################################################################################
 
-#Seleccionando el año 2020
-df_population_2020 = df_population[df_population.index.isin([2020])]
+fig = px.line(df_population) #Grafico de lineas
+fig = df_population.plot(kind='line', title="Population vs Years") #Grafico de lineas, etiquetas.
+fig.show() #Mostrar grafico
 
-#mostrar resultado
-print(df_population_2020)
+############################################################################################################
+                                                  #Grafico de Barras (barplot)
+############################################################################################################
+fig_population_2020 = px.bar(df_population.loc[2020])
+fig_population_2020 = df_population.loc[2020].plot(kind='bar', color={'United States': 'red',
+    'India': 'green',
+    'China': 'blue',
+    'Indonesia': 'orange',
+    'Afghanistan': 'purple'}, title="Population 2020") #Grafico de barras, etiquetas.
+fig_population_2020.show() #Mostrar grafico
+
+############################################################################################################
+                                                #Grafico de Multiples (Barplot)
+############################################################################################################
+fig_population_multiple = px.bar(df_population.loc[[1980, 1990, 2000, 2010,2020]]) #Grafico de barras multiples
+fig_population_multiple = df_population.loc[[1980, 1990, 2000, 2010,2020]].plot(kind='bar', title="Years vs Population") #Grafico de barras multiples, etiquetas.
+fig_population_multiple.show() #Mostrar grafico
+
+#############################################################################################################
+                                                #Grafico de Caja (boxplot)
+#############################################################################################################
+fig_population_box = px.box(df_population) #Grafico de caja
+fig_population_box.show() #Mostrar grafico
+
+##############################################################################################################
+                                                #Grafico de histogram (histogram)
+##############################################################################################################
+fig_population_histogram = px.histogram(df_population, x=['United States','Indonesia']) #Grafico de histograma
+fig_population_histogram.show() #Mostrar grafico
+
+################################################################################################
+                                                #Grafico de Piechart (piechart)
+################################################################################################
+
+fig_population_pie_2020 = px.pie(df_population.loc[2020],
+                                 values=df_population.loc[2020], 
+                                 names=df_population.loc[2020].index, title="Population 2020") #Grafico de piechart.
+fig_population_pie_2020.show() #Mostrar grafico
